6b95c221ae27eb14a59ba0758baee5971a998cdf
natsuko.aoyama <natsuko.aoyama@nijibox.co.jp>
2019-02-06 17:57:45 +0900

    ご飯前にpushしたかった

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import * as React from 'react';
 import axios, {AxiosInstance, AxiosResponse} from 'axios';
 import ReactDOM from 'react-dom';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+
+const styles = theme => ({
+    card: {
+      margin: 48,
+      padding: 10,
+      height: 420
+    },
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+      },
+      input: {
+        margin: theme.spacing.unit,
+    },
+});
 
 
 class App extends React.Component {
 
-    httpClient:AxiosInstance;
 
-    constructor(props:IAppProps){
+    constructor(props){
         super(props);
 
         this.state = {
@@ -38,6 +59,7 @@ class App extends React.Component {
 
         ;
     }
+
     loadAuth(){
         return this.httpClient.get('/auth' , {params:{callback:'http://localhost:3000'}})
             .then(this.commonResponseHandling)
@@ -54,6 +76,17 @@ class App extends React.Component {
             .then(this.commonResponseHandling)
             .then((result)=>{
                 this.setState({departmentList : result});
+            })
+    }
+    ClickDp(e){
+        const target=e.target;
+        const id= target.getAttribute("data-id");
+        console.log(id);
+        return this.httpClient.get('/who/search/',{params:{department_id:id}})
+            .then(this.commonResponseHandling)
+            .then((result)=>{
+                this.setState({departmentList : result.item_list});
+                console.log(result);
             })
     }
     loadUser(){
@@ -92,27 +125,35 @@ class App extends React.Component {
                 alert('エラー発生');
             });
     };
+    
 
     render() {
+        const classes = this.props.classes;
+
         return (
             <div>
-                <ul>
-                    {this.state.departmentList.map((row,index)=>{
-                        return <li key={index}>{row.department_name}</li>;
-                    })}
-                </ul>
 
                 { this.state.isLogin ?
                     <div>
                         <Button variant="raised" color="primary" onClick={this.clickHandler2}>ログインユーザーの情報</Button>
-                        <Button variant="raised" color="primary">検索もしたさがある</Button>
+                        <div>
+                            <p><button onClick={e => this.ClickDp(e)} data-id="4">開発</button></p>
+                            <p><button onClick={e => this.ClickDp(e)} data-id="5">クリエイティブ</button></p>
+                        </div>
 
                         { this.state.user &&
-                        <div>
+                        <Card className={classes.card}>
                             { this.state.user.user_code } { this.state.user.user_name }<br />{ this.state.user.description }
-                            <img src={this.state.user.main_photo_url} />
-                        </div>
+                            <img src={this.state.user.main_photo_url} /><br />
+                            <h3>{ this.state.user.department_name } </h3>
+                        </Card>
                         }
+
+                        <ul>
+                            {this.state.departmentList.map((row,index)=>{
+                                return <li key={index}>{row.user_name}</li>;
+                            })}
+                        </ul>
                     </div>
                     :
                     <p>未ログイン</p>
@@ -124,4 +165,4 @@ class App extends React.Component {
     }
 }
 
-export default App;
+export default withStyles(styles)(App);
